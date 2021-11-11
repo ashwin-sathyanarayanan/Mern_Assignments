c197eb3718312dcca97305a8b4a36da218e4028b
ashwin-sathyanarayanan <84802206+ashwin-sathyanarayanan@users.noreply.github.com>
2021-11-11 12:18:48 -0800

    first_jsx

---
+++ first_jsx/first_jsx/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import New from "./components/New";
+
+function App() {
+  return (
+    <div className="App">
+      <New/>
+    </div>
+  );
+}
+
+export default App;
